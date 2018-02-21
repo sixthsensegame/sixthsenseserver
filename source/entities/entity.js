b6eb38b9e65f8ec43b95107a25382121142cff56
SharkFin <sharkfinpro@hotmail.com>
2018-02-20 16:10:52 -0800

    entity update

--- source/entities/entity.js
+++ source/entities/entity.js
@@ -1,5 +1,13 @@
 module.exports = class entity {
-    constructor(config) {
+    constructor(config, x, y, z, w, h, face) {
         this.config = config;
+
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+        this.h = h;
+
+        this.face = face;
     }
 }
